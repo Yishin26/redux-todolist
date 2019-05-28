69ac8e36cc432b9101a6901670c5a29c032a8f5b
Yishin26 <yishinlin@outlook.com>
2019-05-28 12:03:33 +0800

    Updates

---
+++ precache-manifest.d7e5e4befcdab6bee57857fb339e8bfd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b173f5b35e5f5f06b567a8349051bac3",
+    "url": "/redux-todolist/index.html"
+  },
+  {
+    "revision": "cfea5bbee40649eaddf0",
+    "url": "/redux-todolist/static/js/2.4c694449.chunk.js"
+  },
+  {
+    "revision": "0e2e2f9f686772cbf8e1",
+    "url": "/redux-todolist/static/js/main.8fdecafb.chunk.js"
+  },
+  {
+    "revision": "620c93f6c912dc274a41",
+    "url": "/redux-todolist/static/js/runtime~main.13530c30.js"
+  }
+]);
